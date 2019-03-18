d70e6952ff343023eb869339750627c65a6760bc
Shpak Aliaksandr <shpak-aliaksandr@outlook.com>
2019-03-18 21:40:49 +0300

    1

--- TTR43WEB/Models/Gipermall/ProductsContextQueryable.cs
+++ TTR43WEB/Models/Gipermall/ProductsContextQueryable.cs
@@ -20,21 +20,32 @@ namespace TTR43WEB.Models.Gipermall
         public ProductsContextQueryable(ProductContext ctx) => context = ctx;
 
         public IQueryable<Products> Products => context.Products
-            .Include(e => e.UrlNavigation)
+            .Include(e => e.BarCodeNavigation)
+            .Include(e => e.DimensionNavigation)
+            .Include(e => e.ManufacturingCountryNavigation)
             .Include(e => e.NameNavigation)
-            .Include(e => e.MarkingGoodsNavigation);
+            .Include(e => e.MarkingGoodsNavigation)
+            .Include(e => e.TrademarkNavigation)
+            .Include(e => e.UrlNavigation)
+            ;
 
-        public IQueryable<Name> Names => context.Name;
+        public IQueryable<BarCode> BarCode => context.BarCode;
+        public IQueryable<Dimension> Dimension => context.Dimension;
+        public IQueryable<ManufacturingCountry> ManufacturingCountry => context.ManufacturingCountry;
+        public IQueryable<Name> Name => context.Name;
+        public IQueryable<MarkingGoods> MarkingGoods => context.MarkingGoods;
+        public IQueryable<Trademark> Trademark => context.Trademark;
+        public IQueryable<Url> Url => context.Url;
 
         public Task<int> SaveProduct(Product product)
         {
             if (!context.Products.Any<Products>(
                 p => p.MarkingGoodsNavigation.MarkingGoodsProduct == product.MarkingGoods && 
                 p.Price == product.Price && 
-                p.PriceWithoutDiscount == product.PriceWithoutDiscount))
+                p.PriceWithoutDiscount == product.PriceWithoutDiscount && false))
             {
-
-                //context.Products.Add(product);
+                var tmp = (new Products()).ToProducts(product, context);
+                context.Products.Add(tmp);
             }
             return context.SaveChangesAsync();
         }
---
+++ TTR43WEB/Models/Gipermall/ProductsExtension.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TTR43WEB.Datum;
+
+namespace TTR43WEB.Models.Gipermall
+{
+    public static class ProductsExtension
+    {
+        public static Products ToProducts(this Products products, Product product, ProductContext context)
+        {
+            if (products == null)
+            {
+                throw new ArgumentNullException(nameof(products));
+            }
+
+            /*
+            [Id], 
+            [Date], 
+            [Url], 
+            [Name], 
+            [MarkingGoods], 
+            [Price], 
+            [PriceWithoutDiscount], 
+            [PriceOneKilogram], 
+            [PriceOneLiter], 
+            [Mass], 
+            [Dimension], 
+            [BarCode], 
+            [ManufacturingCountry], 
+            [Trademark]
+            */
+            var _products = new Products();
+
+            if (product.BarCode != null && context.BarCode.Any(e => e.BarCodeProduct == product.BarCode))
+            {
+                context.BarCode.Add(new BarCode { BarCodeProduct = product.BarCode });
+                context.SaveChanges();
+            }
+
+            _products.BarCode = context.BarCode.FirstOrDefault(e => e.BarCodeProduct == product.BarCode).Id;
+
+            _products.Date = product.Date;
+
+            if (product.Url != null && context.Url.Any(e => e.UrlProduct == product.Url))
+            {
+                context.Url.Add(new Url { UrlProduct = product.Url });
+                context.SaveChanges();
+            }
+
+            _products.Url = context.Url.FirstOrDefault(e => e.UrlProduct == product.Url).Id;
+
+
+
+
+            return null;
+        }
+    }
+
+}
